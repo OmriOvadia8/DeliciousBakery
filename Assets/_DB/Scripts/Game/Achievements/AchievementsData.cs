7f56041d565843cb953a898f760e3d71225881c8
OmriOvadia8 <sirsone7@gmail.com>
2023-06-08 17:23:53 +0300

    Removed unused variables

--- Assets/_DB/Scripts/Game/Achievements/AchievementsData.cs
+++ Assets/_DB/Scripts/Game/Achievements/AchievementsData.cs
@@ -14,16 +14,6 @@ namespace DB_Game
         public bool[] Hire500 { get; set; }
         public bool[] Hire1000 { get; set; }
         public bool[] Hire5000 { get; set; }
-        public bool TotalCooked100 { get; set; }
-        public bool TotalCooked1000 { get; set; }
-        public bool TotalCooked5000 { get; set; }
-        public bool TotalCooked10000 { get; set; }
-        public bool TotalCooked20000 { get; set; }
-        public bool TotalHired100 { get; set; }
-        public bool TotalHired1000 { get; set; }
-        public bool TotalHired5000 { get; set; }
-        public bool TotalHired10000 { get; set; }
-        public bool TotalHired20000 { get; set; }
 
         public AchievementData()
         {
@@ -44,5 +34,4 @@ namespace DB_Game
     {
         public AchievementData Achievements { get; set; }
     }
-
 }
